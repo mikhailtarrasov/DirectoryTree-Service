3850d4d85458db032addf9e58c07cfa6adceab52
Михаил <mikhailtarrasov@gmail.com>
2017-05-23 05:40:30 +0500

    Подключена база данных и создана доменная модель

---
+++ DataAccess/DataAccess/Domain/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+using DataAccess.Domain.Entity;
+
+namespace DataAccess.Domain
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("konturDB") { }
+
+        public DbSet<TreeNode> TreeNodes { get; set; }
+    }
+}
---
+++ DataAccess/DataAccess/Domain/Entity/TreeNode.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DataAccess.Domain.Entity
+{
+    public class TreeNode
+    {
+        public string Id { get; set; }
+        public virtual TreeNode Parent { get; set; }
+        public virtual List<TreeNode> Children { get; set; }
+    }
+}
